08d438f415d6b45af2132953a5627d9163bc492f
Wang Haoting <hatsuyukiw@gmail.com>
2017-03-09 21:40:21 +0800

    Fixed a bug in Makefile

---
+++ kernel/main.c
@@ -0,0 +1,31 @@
+#include <include/common.h>
+#include <include/x86.h>
+#include <include/string.h>
+#include <include/stdio.h>
+
+void init_serial();
+void init_i8259();
+void init_idt();
+//void init_timer();
+//void init_vmem();
+
+void printk_test();
+
+void init_all(){
+	init_serial();
+	init_i8259();
+	init_idt();
+	//init_timer();
+//	init_vmem();
+}
+
+int main(void) {
+	init_all();
+
+	printk("Here is main()\n");
+
+	sti();
+
+	assert(0); // should not get here
+	return 0;
+}
